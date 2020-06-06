1fffc6cecf637775b75ba2063d409255d5606528
DanielJDufour <daniel.j.dufour@gmail.com>
2020-06-06 17:28:42 -0400

    updated badge

--- src/components/badges/BadgeCard.js
+++ src/components/badges/BadgeCard.js
@@ -10,26 +10,31 @@ const BadgeCard = ({ entityId }) => {
     const { awarded, duration, image, issuer, name, skill } = badge;
  
     return (
-        <div className="badge-card">
-            <div className="badge-card-img-wrapper">
-                <img src={image}/>
+        <div className="badge-card-wrapper">
+            <div className="badge-card">
+                <div className="badge-card-img-wrapper">
+                    <img src={image}/>
+                </div>
+                <div className="badge-card-title">{name}</div>
+                <div className="badge-card-issuer">{issuer.name}</div>
+                <table className="badge-stats">
+                    <tbody>
+                        <tr className="badge-stats-values">
+                            <td>{skill || '?'}</td>
+                            <td>{awarded || '?'}</td>
+                            <td>{duration || '?'}</td>                        
+                        </tr>
+                        <tr className="badge-stats-keys">
+                            <td>level</td>
+                            <td>awarded</td>
+                            <td>duration</td>
+                        </tr>
+                    </tbody>
+                </table>
             </div>
-            <div className="badge-card-title">{name}</div>
-            <div className="badge-card-issuer">{issuer.name}</div>
-            <table className="badge-stats">
-                <tbody>
-                    <tr className="badge-stats-values">
-                        <td>{skill || '?'}</td>
-                        <td>{awarded || '?'}</td>
-                        <td>{duration || '?'}</td>                        
-                    </tr>
-                    <tr className="badge-stats-keys">
-                        <td>level</td>
-                        <td>awarded</td>
-                        <td>duration</td>
-                    </tr>
-                </tbody>
-            </table>
+            <div className="badge-card-back">
+                
+            </div>
         </div>
     );
 };
