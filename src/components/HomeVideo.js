a1f259a04947b496622e2093be73807e4519a052
viymak <victoria.danya.mak@gmail.com>
2020-06-08 18:14:25 +0000

    homepage

--- src/components/HomeVideo.js
+++ src/components/HomeVideo.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-
-import Background from '../images/home-video.svg';
+import videoContent from '../content/home.md';
 
 const HomeVideo = () => {
     return (
         <section id="home-video">
-            {/* <img src={background} /> */}
+            <img id='home-video-img' src='/static/images/home-video.png' />
+            <div id='overlay'></div>
+            <div id='video-title'>Leveraging the Experience of the Global Community</div>
+            <div id='video-content' dangerouslySetInnerHTML={{__html: videoContent}}></div>
         </section>
     );
 };
--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,8 +1,16 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { RiLoginCircleLine } from 'react-icons/ri';
 
 const Welcome = () => {
-    return <section id="home-welcome">GeoBadges</section>;
+    return (
+        <section id="home-welcome">
+            <h1 id='geo'>GEO</h1>
+            <h1 id='badges'>BADGES</h1>
+            <h3 id='description'>Geographic microcredentials brought to you by the American Geographical Society</h3>
+            <div id='start-collecting'>Start Collecting<RiLoginCircleLine/></div>
+        </section>
+    );
 };
 
 export default Welcome;
