639fc304ba7f6c6f00bebd7cd06fd0aaa49c0430
viymak <victoria.danya.mak@gmail.com>
2020-06-08 21:03:20 +0000

    fixed login bar and home video

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,14 +1,19 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import { RiLoginCircleLine } from 'react-icons/ri';
 
 const Welcome = () => {
     return (
         <section id="home-welcome">
-            <h1 id='geo'>GEO</h1>
-            <h1 id='badges'>BADGES</h1>
-            <h3 id='description'>Geographic microcredentials brought to you by the American Geographical Society</h3>
-            <div id='start-collecting'>Start Collecting<RiLoginCircleLine/></div>
+            <div id="text-content">
+                <h1 id='geo'>GEO</h1>
+                <h1 id='badges'>BADGES</h1>
+                <h3 id='description'>Geographic microcredentials brought to you by the American Geographical Society</h3>
+                <Link to="/badges" id='start-collecting'>
+                    <h4>Start Collecting</h4><RiLoginCircleLine/>
+                </Link>
+            </div>
         </section>
     );
 };
