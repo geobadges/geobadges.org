627e5e45104449dac9e27e045bbba9bb52b33c65
viymak <victoria.danya.mak@gmail.com>
2020-06-10 00:28:09 +0000

    updated home-video

--- src/components/HomeVideo.js
+++ src/components/HomeVideo.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import videoContent from '../content/home.md';
+import FaPlayLight from '../images/play-light.svg';
 
 const HomeVideo = () => {
     return (
-        <section id="home-video">
+        <section id='home-video'>
             <img id='home-video-img' src='/static/images/home-video.png' />
             <div id='overlay'></div>
             <div id='video-title'>Leveraging the Experience of the Global Community</div>
-            <div id='video-content' dangerouslySetInnerHTML={{__html: videoContent}}></div>
+            <div id='span-content'>
+                <div id='video-content' dangerouslySetInnerHTML={{__html: videoContent}}></div>
+                <div id='play-btn' dangerouslySetInnerHTML={{__html: FaPlayLight}}/>
+            </div>
         </section>
     );
 };
