d4653db657607f6cf8eb74c65d59e9281483c063
DanielJDufour <daniel.j.dufour@gmail.com>
2020-07-09 08:32:11 -0500

    added get-all-badges

---
+++ src/actions/get-all-badges.js
@@ -0,0 +1,23 @@
+import axios from "axios";
+
+export default function getAllBadges() {
+  return async function (dispatch, getState) {
+    const store = getState();
+
+    if (store.badges !== null) {
+      console.log("already loaded badges, no need to load again");
+      return;
+    }
+
+    const url = `${process.env.GEOBADGES_FUNCTIONS_ENDPOINT}/get-all-badges`;
+    console.log("url:", url);
+
+    const response = await axios.get(url);
+    console.log("response:", response);
+
+    dispatch({
+      type: "SET_BADGES",
+      data: response.data,
+    });
+  };
+}
