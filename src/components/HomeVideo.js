0eb263e954222aa69a6c2144be61eeb4a766435a
viymak <victoria.danya.mak@gmail.com>
2020-07-13 04:50:28 +0000

    badge carousel

--- src/components/HomeVideo.js
+++ src/components/HomeVideo.js
@@ -1,9 +1,27 @@
-import React from 'react';
+import React, { useRef } from 'react'
 import PropTypes from 'prop-types';
 import videoContent from '../content/home.md';
 import FaPlayLight from '../images/play-light.svg';
+import { FaTimes } from 'react-icons/fa';
+
+
 
 const HomeVideo = () => {
+    const vidRef = useRef(null);
+
+    const handlePlayVideo = () => {
+        const { current: video } = vidRef;
+        video.play();
+        video.setAttribute('active', true);
+    };
+    
+    const escapeVideo = () => {
+        const { current: video } = vidRef;
+        video.setAttribute('active', false);
+        video.load();
+    }
+
+    
     return (
         <section id='home-video'>
             <img id='home-video-img' src='/static/images/home-video.png' />
@@ -11,8 +29,12 @@ const HomeVideo = () => {
             <div id='video-title'>Leveraging the Experience of the Global Community</div>
             <div id='span-content'>
                 <div id='video-content' dangerouslySetInnerHTML={{__html: videoContent}}></div>
-                <div id='play-btn' dangerouslySetInnerHTML={{__html: FaPlayLight}}/>
+                <div id='play-btn' onClick={handlePlayVideo} dangerouslySetInnerHTML={{__html: FaPlayLight}}/>
             </div>
+            <video id='mp4' controls ref={vidRef}>
+                <source src='/static/images/geobadges.mp4' type="video/mp4" />
+            </video> 
+            <div id="video-close-icon" onClick={escapeVideo}><FaTimes/></div>
         </section>
     );
 };
--- src/components/badges/BadgeRow.js
+++ src/components/badges/BadgeRow.js
@@ -1,19 +1,41 @@
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import { useCounter } from 'react-use';
+import Carousel, { Dots } from '@brainhubeu/react-carousel';
+import '@brainhubeu/react-carousel/lib/style.css';
 
 import BadgeCard from './BadgeCard';
 
+
 const BadgeRow = ({ badges, title }) => {
-    const [page, { inc: nextPage, dec: prevPage }] = useCounter(0);
+
+    const pageSize = 5;
+
+    const slides = badges.map(({ entityId }) => {
+        const key = title + '-' + entityId;
+        return <BadgeCard entityId={entityId} key={key}/>;
+    });
+
+    const [page, { inc: nextPage, dec: prevPage, set: setPage }] = useCounter(0);
+
+    const count = badges.length;
+
     return (
         <div className="badge-row">
             <div className="badge-row-title">{title}</div>
             <div className="badge-row-carousel">
-                {badges.map(({ entityId }) => {
-                    const key = title + '-' + entityId;
-                    return <BadgeCard entityId={entityId} key={key}/>;
-                })}
+                <Carousel className="badge-carousel"
+                    onChange={setPage}
+                    value={page * pageSize}
+                    slidesPerPage={5}
+                    slides={slides}
+                    offset={6}
+                />
+                <Dots
+                    number={Math.ceil(count / pageSize)}
+                    onChange={setPage}
+                    value={page}
+                />
             </div>
         </div>
     );
