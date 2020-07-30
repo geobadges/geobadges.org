ccbd754e6d04d5a12a158be61f46708f4eb823aa
DanielJDufour <daniel.j.dufour@gmail.com>
2020-07-29 19:20:54 -0500

    updated

--- src/components/issuer/IssuerStatsCard.js
+++ src/components/issuer/IssuerStatsCard.js
@@ -1,49 +1,55 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import { useDispatch } from 'react-redux';
-import { push } from 'connected-react-router';
-import { useRouteMatch } from 'react-router-dom';
-import classnames from 'classnames';
+import React from "react";
+import PropTypes from "prop-types";
+import { useDispatch } from "react-redux";
+import { push } from "connected-react-router";
+import { useRouteMatch } from "react-router-dom";
+import classnames from "classnames";
 
-import BadgeList from '../badges/BadgeList';
-import Stats from '../Stats';
-import useIssuer from '../../hooks/useIssuer';
+import BadgeList from "../badges/BadgeList";
+import Stats from "../Stats";
+import useIssuer from "../../hooks/useIssuer";
 
 const StatsCard = ({ entityId }) => {
-    const dispatch = useDispatch();
+  const dispatch = useDispatch();
 
-    const match = useRouteMatch();
-    const { issuerId, issuerCard } = match.params;
-    const isIssuerSelected = issuerId === entityId;
-    const isCardSelected = isIssuerSelected && issuerCard === 'stats';
+  const match = useRouteMatch();
+  const { issuerId, issuerCard } = match.params;
+  const isIssuerSelected = issuerId === entityId;
+  const isCardSelected = isIssuerSelected && issuerCard === "stats";
 
-    const { badges, stats } = useIssuer(entityId);
-    const { badges: numBadges, learners, technologies } = stats;
+  const { badges, stats } = useIssuer(entityId);
+  const { badges: numBadges, learners, technologies } = stats;
 
-    const className = classnames(
-        'issuer-stats-card',
-        'issuer-card',
-        { 'issuer-card-selected': isCardSelected },
-        { 'clickable': !isCardSelected }
-    );
-    const handleClick = () => {
-        console.log("clicked connect");
-        if (isIssuerSelected && !isCardSelected) {
-            dispatch(push(`/issuers/${entityId}/stats`));
-        }
-    };
-    return (
-        <li className={className} onClick={handleClick}>
-            <BadgeList badges={badges}/>
-            <Stats
-                keys={['badges','learners','technologies']}
-                values={[numBadges, learners, technologies]}
-            />
-        </li>
-    );
+  const className = classnames(
+    "issuer-stats-card",
+    "issuer-card",
+    { "issuer-card-selected": isCardSelected },
+    { clickable: !isCardSelected }
+  );
+  const handleClick = () => {
+    console.log("clicked connect");
+    if (isIssuerSelected && !isCardSelected) {
+      dispatch(push(`/issuers/${entityId}/stats`));
+    }
+  };
+
+  return (
+    <li className={className} onClick={handleClick}>
+      {numBadges === 0 && (
+        <div className="no-badges-notice">
+          This issuer has not yet created any badges.
+        </div>
+      )}
+      {numBadges > 0 && <BadgeList badges={badges} />}
+      <Stats
+        keys={["badges", "learners", "technologies"]}
+        values={[numBadges, learners, technologies]}
+      />
+    </li>
+  );
 };
 StatsCard.propTypes = {
-    entityId: PropTypes.string.isRequired,
+  entityId: PropTypes.string.isRequired,
 };
 
 export default StatsCard;
