722b0164591147268d752450a99b4c496828228a
viymak <victoria.danya.mak@gmail.com>
2020-08-21 04:32:33 +0000

    resources layout

--- src/components/pages/ResourcesPage.js
+++ src/components/pages/ResourcesPage.js
@@ -1,9 +1,18 @@
 import React from 'react';
-import resourcesContent from '../../content/resources.md';
+import earnerResourcesContent from '../../content/earnersFAQ.md';
+import issuerResourcesContent from '../../content/issuersFAQ.md';
+
 
 const ResourcesPage = () => {
     return (
-        <section id="resources" className="page" dangerouslySetInnerHTML={{__html: resourcesContent }}/>
+        <section id="resources">
+            <div id="faq-image"/>
+                <h3>Earners</h3>
+                <h3>Issuers</h3>
+
+            <div className="page" id="earnerFAQ" dangerouslySetInnerHTML={{__html: earnerResourcesContent }}/>
+            <div className="page" id="issuerFAQ" dangerouslySetInnerHTML={{__html: issuerResourcesContent }}/>
+        </section>
     );
 };
 
