e635067ecc55a9f95fe4a5103a87ab4b5f134386
DanielJDufour <daniel.j.dufour@gmail.com>
2020-10-11 18:34:22 -0400

    updated footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -10,7 +10,7 @@ const FooterSpacing = () => {
 const Footer = () => {
     return (
         <footer id="main-geobadges-footer">
-            <span><img id="gb-logo-in-footer" src="/static/images/geobadges-logo-ags.png"/><span> By the </span><a href="https://americangeo.org/">American Geographical Society</a><span><FooterSpacing/>ags@americangeo.org<FooterSpacing/>Built with </span><span dangerouslySetInnerHTML={{__html: GeoSurgeLogo}}></span><span> by </span><a href="https://github.com/geosurge">GeoSurge</a></span>
+            <span><img id="gb-logo-in-footer" src="/static/images/geobadges-logo-ags.png"/><span className="gb-footer-by-the"> By the </span><a className="gb-footer-ags-link" href="https://americangeo.org/">American Geographical Society</a><span className="gb-footer-more"><FooterSpacing/>ags@americangeo.org<FooterSpacing/>Built with </span><span className="gb-footer-gs-logo" dangerouslySetInnerHTML={{__html: GeoSurgeLogo}}></span><span className="gb-footer-second-by"> by </span><a className="gb-footer-gs-link" href="https://github.com/geosurge">GeoSurge</a></span>
         </footer>
     );
 };
